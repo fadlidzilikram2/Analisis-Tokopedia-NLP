6aee7cf7b4cd7ef4b0d8ae722854062895dfad23
fadlidzilikram2 <fadlidzilikram20@gmail.com>
2026-01-19 12:54:05 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,22 +4,22 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+# Force download di Cloud
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
-    nltk.download('punkt')
-
+    nltk.download('punkt', quiet=True)
 try:
     nltk.data.find('corpora/stopwords')
 except LookupError:
-    nltk.download('stopwords')
+    nltk.download('stopwords', quiet=True)
 
 stop_words = set(stopwords.words("indonesian"))
 
 def preprocess_text(text):
     text = text.lower()
-    text = re.sub(r"\d+", "", text)
+    text = re.sub(r"\\d+", "", text)
     text = text.translate(str.maketrans("", "", string.punctuation))
     tokens = word_tokenize(text)
-    tokens = [w for w in tokens if w not in stop_words]
+    tokens = [w for w in tokens if w not in stop_words and len(w) > 2]
     return " ".join(tokens)
